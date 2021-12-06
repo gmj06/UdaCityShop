0bedd6abcc40160426d3aaf68880f42476e9f1fb
gmj06 <gowrimj2006@gmail.com>
2021-12-06 10:25:48 -0800

    modified adservice-v2 dockerfile and added to github action workflow

--- src/adservice-v2/genproto.sh
+++ src/adservice-v2/genproto.sh
@@ -5,5 +5,5 @@ set -e
 
 # Add the commands to generate the gRPC files
 PROTODIR=../../pb
-pip3 install grpcio-tools
+
 python3 -m  grpc_tools.protoc -I $PROTODIR --python_out=./ --grpc_python_out=./ demo.proto
