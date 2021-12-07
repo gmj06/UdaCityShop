dfcc88f5a578c6bbf9a824d2bc2a92d8d9e75f30
gmj06 <gowrimj2006@gmail.com>
2021-12-06 23:19:36 -0800

    gRPC files

--- src/frontend/genproto.sh
+++ src/frontend/genproto.sh
@@ -22,4 +22,5 @@ protodir=../../pb
 
 # protoc --go_out=plugins=grpc:genproto -I $protodir $protodir/demo.proto
 # --go-grpc_out=genproto
-protoc --go_out=plugins=grpc:genproto  -I $protodir $protodir/demo.proto
+#protoc --go_out=plugins=grpc:genproto  -I $protodir $protodir/demo.proto
+protoc --go-grpc_out=genproto  -I $protodir $protodir/demo.proto
