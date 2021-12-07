5e72307b6c8ae61020f825303a117991af61c897
gmj06 <gowrimj2006@gmail.com>
2021-12-06 21:11:28 -0800

    fixed error in rpc.go

--- src/frontend/rpc.go
+++ src/frontend/rpc.go
@@ -72,5 +72,5 @@ func (fe *frontendServer) getAd(ctx context.Context, ctxKeys []string) ([]*pb.Ad
 	resp, err := pb.NewAdServiceV2Client(fe.adSvcConn).GetAdsV2(ctx, &pb.AdRequest{
 		ContextKeys: ctxKeys,
 	})
-	return resp.GetAdsV2(), errors.Wrap(err, "failed to get ads")
+	return resp.GetAds(), errors.Wrap(err, "failed to get ads")
 }
