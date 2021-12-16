8edc4f0c426230f2597e3f08abe98bef8b5be5aa
gmj06 <gowrimj2006@gmail.com>
2021-12-15 22:23:22 -0800

    fixing errors

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@ ENV["TERM"]="linux"
 Vagrant.configure("2") do |config|
 
   # set the image for the vagrant box
-  config.vm.box = "opensuse/Leap-15.3.x86_64"
+  config.vm.box = "opensuse/Leap-15.2.x86_64"
 
   # st the static IP for the vagrant box
   config.vm.network "private_network", ip: "192.168.56.4"
@@ -21,9 +21,45 @@ Vagrant.configure("2") do |config|
     # install a k3s cluster
     # curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.2+k3s1 K3S_KUBECONFIG_MODE="644" sh -
     curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.3+k3s1 K3S_KUBECONFIG_MODE="644" sh -
+    
     # install iptables
     sudo zypper --non-interactive install iptables
+    
     # install Helm
     curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+    
+    # install golang
+    export GOPATH=$HOME/
+    sudo curl -O https://storage.googleapis.com/golang/go1.17.5.linux-amd64.tar.gz
+    sudo tar -xvf go1.17.5.linux-amd64.tar.gz
+    sudo mv go /usr/local
+    sudo echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+    export PATH=$PATH:/usr/local/go/bin
+    go version 
+
+    # install unzip
+    #sudo zypper --non-interactive install unzip
+    
+    # install protoc
+    #sudo curl -O https://github.com/protocolbuffers/protobuf/releases/download/v3.19.1/protoc-3.19.1-linux-x86_64.zip
+    #sudo unzip protoc-3.19.1-linux-x86_64.zip
+    sudo cp /vagrant/protoc-3.19.1-linux-x86_64/bin/protoc  /usr/bin
+    sudo cp /vagrant/protoc-3.19.1-linux-x86_64/include  /usr/local/
+    sudo echo 'export PATH=$PATH:/usr/bin/protoc' >> ~/.profile
+    chmod a+x /usr/bin/protoc
+    export PATH=$PATH:/usr/bin/protoc
+    protoc --version
+
+    # install protoc-gen-go
+    sudo go install github.com/golang/protobuf/protoc-gen-go@latest
+    chmod a+x $HOME/go/bin
+    sudo echo 'export PATH=$PATH:$HOME/go/bin/protoc-gen-go' >> ~/.profile
+    export PATH=$PATH:$HOME/go/bin/protoc-gen-go
+
+    export GOPATH=$HOME/go
+    export GOROOT=/usr/local/go
+    export PATH=$PATH:$GOPATH/bin
+    export PATH=$PATH:$GOROOT/bin
+
   SHELL
 end
--- src/adservice-v2/adservice-v2_server.py
+++ src/adservice-v2/adservice-v2_server.py
@@ -11,52 +11,56 @@ import grpc
 from grpc_health.v1 import health_pb2
 from grpc_health.v1 import health_pb2_grpc
 
-# generated pb2 files from ../../pb/demo.proto
+#import these pb2 files after generating them
+
 import demo_pb2
 import demo_pb2_grpc
 
 from logger import getJSONLogger
 logger = getJSONLogger('adservice-v2-server')
 
-RANDOM_ADS_TO_SERVE = 2
 
-
-class AdServiceV2(demo_pb2_grpc.ProductCatalogServiceServicer):
-    
-    # Implemet the Ad service business logic
-    def Ads(self, request, context):
-        channel =  grpc.insecure_channel("productcatalogservice:3550")
-        stub = demo_pb2_grpc.ProductCatalogServiceStub(channel)
-        output = stub.ListProducts(demo_pb2.Empty())
-
-        # generating random product id(s) to display with text "AdV2 -  Items with 25% Discount!"
-        random_product_ids =  random.choice(output.products, k=RANDOM_ADS_TO_SERVE)
-        display_text = "AdV2 -  Items with 25% Discount!"
-        random_ads = [demo_pb2.Ad(redirect_url="/product/{}".format(p.id), text=display_text) for p in random_product_ids]
-        return demo_pb2.AdResponse(ads=random_ads)
+class AdServiceV2(demo_pb2_grpc.AdServiceV2Servicer, health_pb2_grpc.HealthServicer):
+    # TODO:
+    # Implement the Ad service business logic
 
     # Uncomment to enable the HealthChecks for the Ad service
     # Note: These are needed for the liveness and readiness probes
     def Check(self, request, context):
-        return health_pb2.HealthCheckResponse(
-            status=health_pb2.HealthCheckResponse.SERVING)
+        return health_pb2.HealthCheckResponse(status=health_pb2.HealthCheckResponse.SERVING)
     
     def Watch(self, request, context):
-        return health_pb2.HealthCheckResponse(
-            status=health_pb2.HealthCheckResponse.UNIMPLEMENTED)
-
-    
+        return health_pb2.HealthCheckResponse(status=health_pb2.HealthCheckResponse.UNIMPLEMENTED)
+    def Ads(self, request, context):
+        # Define the channel and the stub from the pb2 file
+        channel = grpc.insecure_channel("productcatalogservice:3550")
+        stub = demo_pb2_grpc.ProductCatalogServiceStub(channel)
+        response = stub.ListProducts(demo_pb2.Empty())
+        # Find items based on random ID
+        random_id = random.choices(response.products, k=3)
+        # Define the URL and text to be displayed
+        url = "/product/{}"
+        display_text = "AdV2 - Items with 25% Discount!"
+        # Define the ads with the url and text
+        random_ads = [demo_pb2.Ad(redirect_url=url.format(p.id), text=display_text) for p in random_id]
+        return demo_pb2.AdResponse(ads=random_ads)
 
 if __name__ == "__main__":
     logger.info("initializing adservice-v2")
 
-  
+    # TODO:
     # create gRPC server, add the Ad-v2 service and start it
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=200))
-    # Uncomment to add the HealthChecks to the gRPC server to the Ad-v2 service
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=256))
     health_pb2_grpc.add_HealthServicer_to_server(AdServiceV2(), server)
     demo_pb2_grpc.add_AdServiceV2Servicer_to_server(AdServiceV2(), server)
-    print("gRPC Server starting on port 9556...")
-    server.add_insecure_port('[::]:9556')
+    print("Server starting on port 9556...")
+    server.add_insecure_port("[::]:9556")
+    # start the server
     server.start()
-    server.wait_for_termination()
+    # Keep the thread alive
+    try:
+        while True:
+            time.sleep(86400)
+    except KeyboardInterrupt:
+        server.stop(0)
+    
